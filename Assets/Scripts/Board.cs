e6b07a18d6e85af92d36665c39b168203438a6a6
Andrzej Zabski <jedrak32@gmail.com>
2020-11-02 22:11:00 +0100

    Sprites of pieces, grag and drop and board generating

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public GameObject tile;
+    public static Board _instance;
+
+    // Start is called before the first frame update
+
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+
+        _instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start()
+    {
+        for (int i = 0; i < 64; i++)
+        {
+            Vector3 v = new Vector3(i % 8 - 4, i / 8 - 4);
+            GameObject go = Instantiate(tile, v, Quaternion.identity);
+            go.transform.parent = gameObject.transform;
+            if (i % 2 == 0)
+                if ((i / 8) % 2 == 0) go.GetComponent<SpriteRenderer>().color = Color.grey;
+            if (i % 2 == 1)
+                if ((i / 8) % 2 == 1) go.GetComponent<SpriteRenderer>().color = Color.grey;
+        }
+    }
+
+    public Tile[] GetTiles()
+    {
+        return _instance.GetComponentsInChildren<Tile>();
+    }
+
+}
---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    private Board _board;
+    private Tile _currentTile;
+    private Tile _startingTile;
+    private bool _pickedUp = false;
+
+    private void Start()
+    {
+        _board = Board._instance;
+        //_startingTile = Board._instance.GetTiles()[0];
+        _currentTile = _startingTile;
+    }
+
+    private void OnMouseDrag()
+    {
+        _pickedUp = true;
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+    }
+
+    private void OnMouseUp()
+    {
+        _pickedUp = false;
+        float distance = 1.0f;
+        foreach(Tile t in Board._instance.GetTiles())
+        {
+            if (Vector3.Distance(transform.position, t.transform.position) < distance)
+            {
+                distance = Vector3.Distance(transform.position, t.transform.position);
+                _currentTile = t;
+            }
+        }
+
+    }
+
+    private void Update()
+    {
+        if (!_pickedUp && _currentTile != null) transform.position = _currentTile.transform.position;
+    }
+}
