92bfed86b768ca8d31508216d7a4f2919745fc9a
Andrzej Zabski <jedrak32@gmail.com>
2020-11-06 16:01:19 +0100

    pawn movement

--- Assets/Scripts/Movements/PawnMovement.cs
+++ Assets/Scripts/Movements/PawnMovement.cs
@@ -6,16 +6,45 @@ public class PawnMovement : MonoBehaviour, IPieceMovement
 {
     public Tile[] CalculatePossibleMoves(Tile address)
     {
-        Tile[] ret;
+
+        Tile[] up;
         if (address.address.Item2 != 2 && address.address.Item2 != 7)
         {
-            ret = Board.RayCast(address, Vector2Int.up, 2);
+            up = Board.RayCast(address, Vector2Int.up, 2);
 
         }
         else
         {
-            ret = Board.RayCast(address, Vector2Int.up, 3);
+            up = Board.RayCast(address, Vector2Int.up, 3);
         }
+        Tile[] upleft;// Board.RayCast(address, Vector2Int.up + Vector2Int.left, 2);
+        Tile [] upright; //Board.RayCast(address, Vector2Int.up + Vector2Int.right, 2);
+        Tile [] buff = Board.RayCast(address, Vector2Int.up + Vector2Int.left, 2);
+        if (buff[0].Taken)
+        {
+            upleft = new Tile[1];
+            buff.CopyTo(upleft, 0);
+        }
+        else
+        {
+            upleft = new Tile[0];
+        }
+
+        buff = Board.RayCast(address, Vector2Int.up + Vector2Int.right, 2);
+        if (buff[0].Taken)
+        {
+            upright = new Tile[1];
+            buff.CopyTo(upright, 0);
+        }
+        else
+        {
+            upright = new Tile[0];
+        }
+
+        Tile[] ret = new Tile[up.Length + upleft.Length + upright.Length];
+        up.CopyTo(ret, 0);
+        upleft.CopyTo(ret, up.Length);
+        upright.CopyTo(ret, up.Length + upleft.Length);
 
         return ret;
     }
