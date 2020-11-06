9910dd7737784b19ba932dd4a92bb8e9a7e71c95
Andrzej Zabski <jedrak32@gmail.com>
2020-11-06 14:53:38 +0100

    initial pawn movement

---
+++ Assets/Scripts/Movements/PawnMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PawnMovement : MonoBehaviour, IPieceMovement
+{
+    public Tile[] CalculatePossibleMoves(Tile address)
+    {
+        Tile[] ret;
+        if (address.address.Item2 != 2 && address.address.Item2 != 7)
+        {
+            ret = Board.RayCast(address, Vector2Int.up, 2);
+
+        }
+        else
+        {
+            ret = Board.RayCast(address, Vector2Int.up, 3);
+        }
+
+        return ret;
+    }
+
+}
